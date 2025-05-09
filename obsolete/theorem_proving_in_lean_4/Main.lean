bbe8f47b8708e0158bad8c2e695bc2af16301fc4
khanh <khanh.nguyen.contact@gmail.com>
2025-05-10 02:20:49 +0800

    auto_push

--- obsolete/theorem_proving_in_lean_4/Main.lean
+++
@@ -1,9 +0,0 @@
-import theorem_proving.auxiliary_subgoals
-import theorem_proving.calculation_proof
-import theorem_proving.calculation_proof_for_transitivity
-import theorem_proving.tactic
-import theorem_proving.inductive_type
-
-
-def main : IO Unit :=
-  IO.println s!"Hello, world!"
--- theorem_proving/type_theory_1/predicate_logic.lean
+++ theorem_proving/type_theory_1/predicate_logic.lean
@@ -1,3 +1,3 @@
 namespace PredicateLogic
-
+  -- TODO
 end PredicateLogic
