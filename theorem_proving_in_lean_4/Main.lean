6292937aa2c7142e4adc5b1eb96e1b1f43c7f484
khanh <khanh.nguyen.contact@gmail.com>
2025-05-09 13:38:38 +0800

    auto_push

--- theorem_proving_in_lean_4/Main.lean
+++ theorem_proving_in_lean_4/Main.lean
@@ -1,5 +1,7 @@
 import theorem_proving.auxiliary_subgoals
 import theorem_proving.calculation_proof
+import theorem_proving.calculation_proof_for_transitivity
+import theorem_proving.tactic
 
 
 def main : IO Unit :=
