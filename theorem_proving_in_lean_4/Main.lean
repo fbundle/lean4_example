921eeb52d3ff20da4d8798b7f469e71dfe86df28
khanh <khanh.nguyen.contact@gmail.com>
2025-05-09 21:45:42 +0800

    auto_push

--- theorem_proving_in_lean_4/Main.lean
+++ theorem_proving_in_lean_4/Main.lean
@@ -2,6 +2,7 @@ import theorem_proving.auxiliary_subgoals
 import theorem_proving.calculation_proof
 import theorem_proving.calculation_proof_for_transitivity
 import theorem_proving.tactic
+import theorem_proving.inductive_type
 
 
 def main : IO Unit :=
---
+++ theorem_proving_in_lean_4/theorem_proving/inductive_type.lean
@@ -0,0 +1,17 @@
+
+
+inductive Weekday where
+ | sunday : Weekday
+ | monday : Weekday
+ | tuesday : Weekday
+ | wednesday : Weekday
+ | thursday : Weekday
+ | friday : Weekday
+ | saturday : Weekday
+#check Weekday.sunday
+#check Weekday.monday
+
+open Weekday
+
+#check sunday
+#check monday
