0a1460f0f8338ef1c083b552d3f092ce96f1b5a5
khanh <khanh.nguyen.contact@gmail.com>
2025-05-10 21:05:43 +0800

    auto_push

--- theorem_proving/Main.lean
+++ theorem_proving/Main.lean
@@ -3,7 +3,6 @@ import type_theory_1.dependent_type_theory
 import type_theory_1.inductive_type
 import type_theory_1.predicate_logic
 
-
 import tactic_2.calculation_proof
 import tactic_2.calculation_proof_for_transitivity
 import tactic_2.auxiliary_subgoals
