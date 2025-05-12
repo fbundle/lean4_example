f872b3486939c60f2f3d6fed92932f4edce4319f
khanh <khanh.nguyen.contact@gmail.com>
2025-05-12 16:07:42 +0800

    auto_push

--- theorem_proving/Main.lean
+++ theorem_proving/Main.lean
@@ -8,12 +8,9 @@ import tactic.calculation_proof_for_transitivity
 import tactic.auxiliary_subgoals
 import tactic.tactic
 
-import Mathlib.Logic.Basic
 
-def l5 {p q: Prop}: ¬ (p → ¬ q) → p ∧ q := by
-  intro h
-  have h₁ : ¬(p → ¬q) ↔ p ∧ ¬¬q := Mathlib.Logic.Basic.not_imp
-  exact sorry
+
+
 
 def main : IO Unit :=
   IO.println s!"Hello, World!"
