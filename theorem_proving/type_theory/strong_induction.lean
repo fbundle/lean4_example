cf79fcc6b7386b9c10672abb0a94e6d7b4121e02
khanh <khanh.nguyen.contact@gmail.com>
2025-05-12 19:23:54 +0800

    auto_push

--- theorem_proving/type_theory/strong_induction.lean
+++ theorem_proving/type_theory/strong_induction.lean
@@ -1,7 +1,7 @@
 import Mathlib.Tactic.Tauto
+import Mathlib.Tactic.Induction
 
 section StrongInduction
-
 def is_prime (n: Nat): Prop := by
   exact (2 ≤ n) ∧ (∀ (m: Nat), m ∣ n → ¬ (2 ≤ m ∧ m < n))
 
@@ -49,30 +49,31 @@ def l4: ∀ (m n l: Nat), m ∣ n → n ∣ l → m ∣ l := by
 theorem prime_decomposition: ∀ (n: Nat), (2 ≤ n) → ∃ (m: Nat), (is_prime m) ∧ (m ∣ n) := by
   intro n -- `n: Nat`
   -- `ih` strong induction hypothesis
-  have ih : ∀ (m: Nat), (m < n) → (m ≥ 2) → ∃ (l: Nat), (is_prime l) ∧ (l ∣ m) := by sorry
-  intro h₁ -- `h₁: 2 ≤ n`
-  by_cases h₂ : is_prime n
-  case pos => -- `h₂: is_prime n`
-    exact Exists.intro n (And.intro h₂ (l3 n))
-  case neg => -- h₂: `¬is_prime n`
-    have h₃ : ¬ (∀ (m: Nat), m ∣ n → ¬ (2 ≤ m ∧ m < n)) := l2 h₂ h₁
-    have h₄ : ∃ (m: Nat), ¬(m ∣ n → ¬ (2 ≤ m ∧ m < n)) := by exact l1 h₃
-    cases h₄ with
-      | intro w hw => -- `hw : ¬(w ∣ n → ¬ (1 < w ∧ w < n)`
-        have hw₀ : w ∣ n ∧ 1 < w ∧ w < n := l5 hw
-        have hw₁ : w ∣ n := hw₀.left
-        have hw₂ : 1 < w ∧ w < n := hw₀.right
-        by_cases hw₃ : is_prime w
-        case pos => -- `hw₄ : is_prime w`
-          exact Exists.intro w (And.intro hw₃ hw₁)
-        case neg => -- `hw₄ : ¬ is_prime w`
-          have hv₀ : (w < n) → (w ≥ 2) → ∃ (l: Nat), (is_prime l) ∧ (l ∣ w) := ih w
-          have hv₁ : ∃ l, is_prime l ∧ l ∣ w := (hv₀ hw₂.right) hw₂.left
-          cases hv₁ with
-          | intro v hv => -- `hv : is_prime v ∧ v ∣ w`
-            have hv₁: is_prime v := hv.left
-            have hv₂: v ∣ w := hv.right
-            have v_divides_n := l4 v w n hv₂ hw₁
-            exact Exists.intro v (And.intro hv₁ v_divides_n)
+  induction' n using Nat.strong_induction_on with n ih
+  -- have ih : ∀ (m: Nat), (m < n) → (m ≥ 2) → ∃ (l: Nat), (is_prime l) ∧ (l ∣ m) := by sorry
+    intro h₁ -- `h₁: 2 ≤ n`
+    by_cases h₂ : is_prime n
+    case pos => -- `h₂: is_prime n`
+      exact Exists.intro n (And.intro h₂ (l3 n))
+    case neg => -- h₂: `¬is_prime n`
+      have h₃ : ¬ (∀ (m: Nat), m ∣ n → ¬ (2 ≤ m ∧ m < n)) := l2 h₂ h₁
+      have h₄ : ∃ (m: Nat), ¬(m ∣ n → ¬ (2 ≤ m ∧ m < n)) := by exact l1 h₃
+      cases h₄ with
+        | intro w hw => -- `hw : ¬(w ∣ n → ¬ (1 < w ∧ w < n)`
+          have hw₀ : w ∣ n ∧ 1 < w ∧ w < n := l5 hw
+          have hw₁ : w ∣ n := hw₀.left
+          have hw₂ : 1 < w ∧ w < n := hw₀.right
+          by_cases hw₃ : is_prime w
+          case pos => -- `hw₄ : is_prime w`
+            exact Exists.intro w (And.intro hw₃ hw₁)
+          case neg => -- `hw₄ : ¬ is_prime w`
+            have hv₀ : (w < n) → (w ≥ 2) → ∃ (l: Nat), (is_prime l) ∧ (l ∣ w) := ih w
+            have hv₁ : ∃ l, is_prime l ∧ l ∣ w := (hv₀ hw₂.right) hw₂.left
+            cases hv₁ with
+            | intro v hv => -- `hv : is_prime v ∧ v ∣ w`
+              have hv₁: is_prime v := hv.left
+              have hv₂: v ∣ w := hv.right
+              have v_divides_n := l4 v w n hv₂ hw₁
+              exact Exists.intro v (And.intro hv₁ v_divides_n)
 
 end StrongInduction
