7113fe9e02cc26edcac242112f080ac48e2beaba
khanh <khanh.nguyen.contact@gmail.com>
2025-05-14 01:21:36 +0800

    auto_push

---
+++ mathematics/basic/basic.lean
@@ -0,0 +1,34 @@
+import Mathlib.Algebra.Ring.Basic
+
+section Ring
+  -- let `R` be an associative unital ring
+  variable (R : Type*) [Ring R]
+
+  #check (add_assoc : ∀ a b c : R, a + b + c = a + (b + c))
+  #check (add_comm : ∀ a b : R, a + b = b + a)
+  #check (zero_add : ∀ a : R, 0 + a = a)
+  #check (neg_add_cancel : ∀ a : R, -a + a = 0)
+  #check (mul_assoc : ∀ a b c : R, a * b * c = a * (b * c))
+  #check (mul_one : ∀ a : R, a * 1 = a)
+  #check (one_mul : ∀ a : R, 1 * a = a)
+  #check (mul_add : ∀ a b c : R, a * (b + c) = a * b + a * c)
+  #check (add_mul : ∀ a b c : R, (a + b) * c = a * c + b * c)
+
+end Ring
+
+section CommRing
+  -- let `R` be an commutative unital ring
+  variable (R : Type*) [CommRing R]
+
+  #check (add_assoc : ∀ a b c : R, a + b + c = a + (b + c))
+  #check (add_comm : ∀ a b : R, a + b = b + a)
+  #check (zero_add : ∀ a : R, 0 + a = a)
+  #check (neg_add_cancel : ∀ a : R, -a + a = 0)
+  #check (mul_assoc : ∀ a b c : R, a * b * c = a * (b * c))
+  #check (mul_one : ∀ a : R, a * 1 = a)
+  #check (one_mul : ∀ a : R, 1 * a = a)
+  #check (mul_add : ∀ a b c : R, a * (b + c) = a * b + a * c)
+  #check (add_mul : ∀ a b c : R, (a + b) * c = a * c + b * c)
+  #check (mul_comm: ∀ a b : R, a * b = b * a)
+
+end CommRing
