550b0e8d569293503797efef36831474883ce82f
khanh <khanh.nguyen.contact@gmail.com>
2025-05-14 01:28:28 +0800

    auto_push

--- mathematics/basic/basic.lean
+++ mathematics/basic/basic.lean
@@ -14,6 +14,21 @@ section Ring
   #check (mul_add : ∀ a b c : R, a * (b + c) = a * b + a * c)
   #check (add_mul : ∀ a b c : R, (a + b) * c = a * c + b * c)
 
+  theorem neg_add_cancel_left1 (a b : R) : -a + (a + b) = b := by
+    calc
+      - a + (a + b) = -a + a + b := by rw[← add_assoc]
+      _ = 0 + b := by rw [neg_add_cancel]
+      _ = b := by rw [zero_add]
+
+  theorem add_neg_cancel_right1 (a b : R) : a + b + -b = a := by
+    calc
+      a + b + -b = a + (b + -b) := by rw[add_assoc]
+      _ = a + (-b + b) := by rw [add_comm]
+      _ = a + 0 := by rw [neg_add_cancel]
+      _ = 0 + a := by rw [add_comm]
+      _ = a := by rw [zero_add]
+
+
 end Ring
 
 section CommRing
