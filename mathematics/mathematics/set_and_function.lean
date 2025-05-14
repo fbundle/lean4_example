1d262ed95f6ade9e9e56757f982cce489dbc60f4
khanh <khanh.nguyen.contact@gmail.com>
2025-05-15 00:36:18 +0800

    auto_push

--- mathematics/mathematics/set_and_function.lean
+++ mathematics/mathematics/set_and_function.lean
@@ -1,4 +1,7 @@
 import Mathlib.Data.Set.Basic
+
+section Set
+
 variable {α : Type*}
 variable (s t u : Set α)
 open Set
@@ -9,3 +12,5 @@ example : s ∩ t = t ∩ s := by
   constructor
   · rintro ⟨xs, xt⟩; exact ⟨xt, xs⟩
   · rintro ⟨xt, xs⟩; exact ⟨xs, xt⟩
+
+end Set
