09963e28e158ef51c0bede7916d9623dd4775f6c
khanh <khanh.nguyen.contact@gmail.com>
2025-05-15 00:35:49 +0800

    auto_push

--- mathematics/mathematics/set_and_function.lean
+++ mathematics/mathematics/set_and_function.lean
@@ -1,7 +1,10 @@
-import Mathlib.Data.Set.Defs
+import Mathlib.Data.Set.Basic
+variable {α : Type*}
+variable (s t u : Set α)
+open Set
 
 example : s ∩ t = t ∩ s := by
-  ext x
+  ext x -- make goal `x ∈ s ∩ t ↔ x ∈ t ∩ s`
   simp only [mem_inter_iff]
   constructor
   · rintro ⟨xs, xt⟩; exact ⟨xt, xs⟩
