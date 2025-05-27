e1b426812a8558ef2102e72fac3be57bf0aa4538
nguyen_ngoc_khanh <nguyen_ngoc_khanh@i2r.a-star.edu.sg>
2025-05-27 13:57:55 +0800

    auto_push

--- echo_line_app/json/Json.lean
+++ echo_line_app/json/Json.lean
@@ -19,13 +19,11 @@ def head2 (s: String): Option Char × Option Char :=
     | _ => (some s.front, some (s.drop 1).front)
 
 
-
+-- helper
 def parseExactString (s₁: String) (s: String): String × Bool :=
   if s.startsWith s₁
   then (s.drop s₁.length, true)
   else (s, false) -- string does not match
-
-def parseExactChar (c: Char) (s: String): String × Bool := parseExactString c.toString s
 
 def parseDigit (s: String): String × Option Char :=
   match head s with
@@ -38,21 +36,25 @@ def parseDigit (s: String): String × Option Char :=
 -- parse json
 
 def parseString(s: String): String × Option Json :=
-  let rec loop (s: String) (acc: String) : String × String :=
+  let rec loop (s: String) (acc: String) : String × Option String :=
     match head2 s with
-      | (none, none) => (s, acc) -- parse error
-      | (some '"', none) => (s.drop 1, acc) -- end of string
+      | (none, none) => (s, none) -- parse error
+      | (some '"', none) => (s.drop 1, some acc) -- end of string
       | (some '\\', some '"') => loop (s.drop 2) (acc ++ "\"")
       | (some '\\', some '\\') => loop (s.drop 2) (acc ++ "\\")
       | (some c, _) => loop (s.drop 1) (acc ++ c.toString)
-      | _ => (s, acc) -- dummy case to satisfy pattern matching
+      | _ => (s, none) -- dummy case to satisfy pattern matching
     decreasing_by all_goals sorry
 
+  let consumeDoubleQuote (s: String): String × Bool := parseExactString "\"" s
+
   -- we can only parse a string if it starts with a double quote
-  match parseExactChar '"' s with
+  match consumeDoubleQuote s with
     | (s, true) => -- string must start with double quote
-      let (s, s₁) := loop s ""
-      (s, some (Json.string s₁))
+      let (s, o_acc) := loop s ""
+      match o_acc with
+        | none => (s, none)
+        | some acc => (s, some (Json.string acc))
     | _ => (s, none) -- parse error
 
 
@@ -60,22 +62,26 @@ def parseString(s: String): String × Option Json :=
 #eval! parseString "\"Hello, \\\"World!\\\"\" 123"
 
 def parseInteger (s: String): String × Option Json :=
-  let parseIntegerString (s: String): String × String :=
-  -- parse a number, which may start with a minus sign
-    let rec parseAllDigits (s: String) (acc: String): String × String :=
-      match parseDigit s with
-        | (s, some c) => parseAllDigits s (acc ++ c.toString)
-        | (s, none) => (s, acc)
-    decreasing_by all_goals sorry
+  let rec parseAllDigits (s: String) (acc: String): String × Option Int :=
+    match parseDigit s with
+      | (s, some c) => parseAllDigits s (acc ++ c.toString)
+      | (s, none) =>
+        match acc.toInt? with
+          | some abs => (s, some abs)
+          | none => (s, none)
+  decreasing_by all_goals sorry
 
-    match parseExactChar '-' s with
-      | (s, true) => parseAllDigits s "-"
-      | _ => parseAllDigits s ""
+  let consumeSign (s: String) : String × Int :=
+    match parseExactString "-" s with
+      | (s, true) => (s, -1)
+      | _ => (s, 1)
 
-  let (s, c) := parseIntegerString s
-  match c.toInt? with
-    | some n => (s, some (Json.number n))
-    | none => (s, none) -- parse error
+  let (s, sign) := consumeSign s
+  let (s, o_abs) := parseAllDigits s ""
+  match o_abs with
+    | some abs => (s, some (Json.number (sign * abs)))
+    | none => (s, none)
+
 
 #eval! parseInteger "-12345"
 #eval! parseInteger "67890"
@@ -92,3 +98,18 @@ def parseConst (s: String): String × Option Json :=
 #eval! parseConst "null"
 #eval! parseConst "true"
 #eval! parseConst "false"
+
+def parseArray(s: String): String × Option Json :=
+  let rec loop (s: String) (acc: List Json): String × Option List Json :=
+    match parseJson s with
+      | (s, some json) =>
+
+
+
+  match parseExactString "[" with
+    | (s, true) =>
+    | _ => (s, none)
+
+
+def parseJson(s: String): String × Option Json :=
+  (s, none)
