ffb1e23ed3690a742d169c41fbd835eb78464f5a
nguyen_ngoc_khanh <nguyen_ngoc_khanh@i2r.a-star.edu.sg>
2025-05-27 15:18:10 +0800

    auto_push

--- echo_line_app/json/Json.lean
+++ echo_line_app/json/Json.lean
@@ -23,8 +23,9 @@ def consumeExactString (s₁: String) (s: String): String × Bool :=
   then (s.drop s₁.length, true)
   else (s, false) -- string does not match
 
--- parse json
-def parseString(s: String): String × Option Json :=
+-- parse literal
+
+def parseStringLiteral(s: String): String × Option String :=
   let rec parseContent (s: String) (acc: String) : String × Option String :=
     match head2 s with
       | (none, none) => (s, none) -- parse error
@@ -43,14 +44,10 @@ def parseString(s: String): String × Option Json :=
       let (s, o_acc) := parseContent s ""
       match o_acc with
         | none => (s, none)
-        | some acc => (s, some (Json.string acc))
+        | some acc => (s, some acc)
     | _ => (s, none) -- parse error
 
-
-#eval! parseString "\"Hello, World!\""
-#eval! parseString "\"Hello, \\\"World!\\\"\" 123"
-
-def parseInteger (s: String): String × Option Json :=
+def parseIntegerLiteral (s: String): String × Option Int :=
   let consumeDigit (s: String): String × Option Char :=
     match head s with
       | some c =>
@@ -75,46 +72,67 @@ def parseInteger (s: String): String × Option Json :=
   let (s, sign) := consumeSign s
   let (s, o_abs) := parseNatural s ""
   match o_abs with
-    | some abs => (s, some (Json.number (sign * abs)))
+    | some abs => (s, some (sign * abs))
     | none => (s, none)
 
 
-#eval! parseInteger "-12345"
-#eval! parseInteger "67890"
-
-def parseConst (s: String): String × Option Json :=
-  if s.startsWith "null"
-  then (s.drop 4, some Json.null)
-  else if s.startsWith "true"
-  then (s.drop 4, some (Json.bool true))
-  else if s.startsWith "false"
-  then (s.drop 5, some (Json.bool false))
-  else (s, none) -- parse error, return none
-
-#eval! parseConst "null"
-#eval! parseConst "true"
-#eval! parseConst "false"
-
-def parseArray(s: String): String × Option Json :=
-  let rec parseContent (s: String) (acc: List Json): String × Option List Json :=
-    match parseJson s with
-      | (s, some json) =>
-        let acc := acc ++ json
-        match head s with
-          | some ',' => parseContent (s.drop 1) acc
-          | some ']' => ((s.drop 1) acc)
-          | _ => (s, none)
-      | _ => (s, none) -- parse error
-    decreasing_by all_goals sorry
-
-  let consumeOpenBracket (s: String): String × Bool := consumeExactString "[" s
-
-  match consumeOpenBracket with
-    | (s, true) =>
-      let (s, acc) := parseContent s []
-      (s, Json.array acc)
-    | _ => (s, none)
 
 
-def parseJson(s: String): String × Option Json :=
-  (s, none)
+-- parse json
+mutual
+
+  def parseStringJson(s: String): String × Option Json :=
+    let (s, o_acc) := parseStringLiteral s
+    match o_acc with
+      | some acc => (s, (Json.string acc))
+      | none => (s, none)
+
+  def parseIntegerJson(s: String): String × Option Json :=
+    let (s, o_acc) := parseIntegerLiteral s
+    match o_acc with
+      | some acc => (s, (Json.number acc))
+      | none => (s, none)
+
+  def parseConstantJson (s: String): String × Option Json :=
+    if s.startsWith "null"
+    then (s.drop 4, some Json.null)
+    else if s.startsWith "true"
+    then (s.drop 4, some (Json.bool true))
+    else if s.startsWith "false"
+    then (s.drop 5, some (Json.bool false))
+    else (s, none) -- parse error, return none
+
+
+
+  def parseArrayJson(s: String): String × Option Json :=
+    let rec parseContent (s: String) (acc: List Json): String × Option (List Json) :=
+      match parseJson s with
+        | (s, some json) =>
+          let acc := acc ++ [json]
+          match head s with
+            | some ',' => parseContent (s.drop 1) acc
+            | some ']' => ((s.drop 1), acc)
+            | _ => (s, none)
+        | _ => (s, none) -- parse error
+      decreasing_by all_goals sorry
+
+    let consumeOpenBracket (s: String): String × Bool := consumeExactString "[" s
+
+    match consumeOpenBracket s with
+      | (s, true) =>
+        let (s, o_acc) := parseContent s []
+        match o_acc with
+          | some acc => (s, Json.array acc)
+          | none => (s, none)
+      | _ => (s, none)
+
+  def parseObjectJson (s: String): String × Option Json :=
+    let consumeOpenBrace (s: String): String × Bool := consumeExactString "{" s
+
+    (s, none)
+
+
+  def parseJson(s: String): String × Option Json :=
+    (s, none)
+
+end
