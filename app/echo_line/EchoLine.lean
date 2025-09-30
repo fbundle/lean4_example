67b1bf396295d364604f1e1fe3c8e09cc2ecb242
nguyen ngoc khanh <nguyen_ngoc_khanh@i2r.a-star.edu.sg>
2025-09-30 15:59:16 +0800

    auto_push

--- app/echo_line/EchoLine.lean
+++ app/echo_line/EchoLine.lean
@@ -5,9 +5,6 @@ universe u
 
 def apply_func (α : Type u) : Type u := α → String → α × String
 
-def app (α : Type u) : Type u := α × apply_func α
-
-
 partial def main_loop (apply: apply_func α) (state: α) : IO Unit := do
   let stdin ← IO.getStdin
   let stdout ← IO.getStdout
