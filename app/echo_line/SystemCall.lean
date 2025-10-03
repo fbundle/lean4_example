ef27de9a29432d5386d0b8d0ec3332dd412e7a62
khanh <khanh.nguyen.contact@gmail.com>
2025-10-03 13:37:56 +0800

    auto_push

--- app/echo_line/SystemCall.lean
+++ app/echo_line/SystemCall.lean
@@ -3,11 +3,28 @@ import Lean.Data.Json
 namespace EchoLine
 
 
-def SysCallReadFileText (filename: String): Lean.Json :=
+def ReadFileTextRequest (filename: String): Lean.Json :=
   Lean.Json.mkObj [
-    ⟨"syscall", "read_file_text"⟩,
-    ⟨"syscall", filename⟩
+    ⟨"request", "read_file_text"⟩,
+    ⟨"filename", filename⟩
   ]
+
+def ReadFileTextResponse (json: Lean.Json): Except String String :=
+  match json.getObjVal? "error" with
+    | Except.error err => Except.error err -- error field does not exist
+    | Except.ok err =>
+      match err with
+        | Lean.Json.null => -- error field null
+          match json.getObjVal? "content" with
+            | Except.error err => Except.error err -- content field does not exist
+            | Except.ok content =>
+              match content.getStr? with
+                | Except.error err => Except.error err -- content is not a string
+                | Except.ok str => Except.ok str
+        | _ => Except.error err.compress -- error field non-null
+
+
+
 
 
 end EchoLine
