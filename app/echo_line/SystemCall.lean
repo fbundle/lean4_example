dbd8edad8a4e8cb7b96779ce189d56f779b5767c
khanh <khanh.nguyen.contact@gmail.com>
2025-10-03 13:48:58 +0800

    auto_push

--- app/echo_line/SystemCall.lean
+++ app/echo_line/SystemCall.lean
@@ -5,27 +5,24 @@ namespace EchoLine
 open Lean.Json
 
 def ReadFileTextRequest (filename: String): Lean.Json :=
-  Lean.Json.mkObj [
+  mkObj [
     ⟨"request", "read_file_text"⟩,
     ⟨"filename", filename⟩
   ]
 
-def ReadFileTextResponse (json: Lean.Json): Except String String :=
-  match json.getObjVal? "error" with
-    | Except.error err => Except.error err -- error field does not exist
-    | Except.ok err =>
-      match err with
-        | Lean.Json.null => -- error field null
-          match json.getObjVal? "content" with
-            | Except.error err => Except.error err -- content field does not exist
-            | Except.ok content =>
-              match content.getStr? with
-                | Except.error err => Except.error err -- content is not a string
-                | Except.ok str => Except.ok str
-        | _ => Except.error err.compress -- error field non-null
+def ReadFileTextResponse2 (json: Lean.Json): Except String String := do
+  let error ← json.getObjVal? "error"
+  if error != null then
+    Except.error error.compress
+  else
+    let content ← json.getObjVal? "content"
+    let contentStr ← content.getStr?
+    return contentStr
 
 
-
-
+#eval ReadFileTextResponse2 (mkObj [
+  ⟨ "error", null ⟩,
+  ⟨ "content", "file content" ⟩,
+])
 
 end EchoLine
